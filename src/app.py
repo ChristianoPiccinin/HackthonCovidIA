9afcb0b50157a59098c17dc20382148f9ccfe264
Christiano Piccinin <christiano.piccinin@gmail.com>
2021-06-05 12:29:14 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,54 +1,59 @@
-import streamlit as st
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array, load_img, array_to_img
-from tensorflow.keras.preprocessing import image 
-from PIL import Image,ImageOps
+import streamlit as st
+import cv2
+from PIL import Image, ImageOps
 import numpy as np
+from typing import Tuple
+
+st.set_option('deprecation.showfileUploaderEncoding', False)
+@st.cache(allow_output_mutation=Tuple)
+
+def load_model():
+    model = tf.keras.models.load_model('model.h5')
+    return model
+
+with st.spinner('Carregando modelo..'):
+    model = load_model()
 
 
-model = load_model('model.h5')
+def import_and_predict(image_data, model):
+    size = (299,299)    
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    image = np.asarray(image)
+    img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img_reshape = img[np.newaxis,...]
+    prediction = model.predict(img_reshape)
+    return prediction
+    
 
 def main():
-
-    #st.set_page_config(layout="wide")
-
+    
+    class_names = ['covid','normal','lung_opacity','pneumonia']
+       
     html_template = """
-        <div style ="background-color:#1B9CFC;padding:5px">
-        <h2 style ="color:white;text-align:center;">DIAGNOSTICO DE COVID-19 POR RAIO-X</h2>
-        <h4 style ="color:white;text-align:center;">Christiano Piccinin, Lucenildo Cerqueira e Hugo Brandão</h3>
-        </div>
-    """
+            <div style ="background-color:#1B9CFC;padding:5px">
+            <h2 style ="color:white;text-align:center;">DIAGNOSTICO DE COVID-19 POR RAIO-X</h2>
+            <h4 style ="color:white;text-align:center;">Christiano Piccinin, Lucenildo Cerqueira e Hugo Brandão</h3>
+            </div>
+        """
 
     # Função do stramlit que faz o display da webpage
     st.markdown(html_template, unsafe_allow_html = True)
 
     st.write('')
-    st.write('1º Clique sobre o botão Browse files e submeta a imagem para ser analisada')
-    st.set_option('deprecation.showfileUploaderEncoding', False)
-    uploaded_file = st.file_uploader("", type=["png", "jpg", "jpeg"], accept_multiple_files=False, key=None, help="Envie uma imagem de raio-x do pulmão para nosso sistema de IA. Só é permitido uma imagem por vez!")
     
+
+    uploaded_file = st.file_uploader("", type=["png", "jpg", "jpeg"],
+                                 accept_multiple_files=False,
+                                 key=None)
+
     if uploaded_file is not None:
-        Img = Image.open(uploaded_file)
-        st.image(Img, caption='Imagem Carregada', width=600)
-        st.write(uploaded_file)
-        st.write(uploaded_file.name)
-        
-        #x = f'C:/Users/devch/OneDrive/Área de Trabalho/Hackthon_COVID/src/COVID/{uploaded_file.name}'
-        img = image.load_img(uploaded_file.name, target_size=(299, 299))
-        img = image.img_to_array(img)
-        #img = img/255
-        img = img.reshape(1,299,299,3)
-
-        prediction = model.predict(img)
-
-        if(np.argmax(prediction) == 3.0):
-            st.error('A probabilidade de ser COVID-19 é  {} %'.format(prediction))
-        else:
-            st.success(prediction)
-            st.success(prediction[0])
-            st.success('Não existe a probabilidade de ser COVID-19')
-        
+        image = Image.open(uploaded_file)
+        st.image(image, use_column_width=True)
+        predictions = import_and_predict(image, model)
+        score = tf.nn.softmax(predictions[0])
+        #st.write(score)
+        st.success('A probabilidade de ser {} é {:.2f} %'.format(class_names[np.argmax(score)], 100 * np.max(score)))
 
 if __name__=='__main__':
     main()
